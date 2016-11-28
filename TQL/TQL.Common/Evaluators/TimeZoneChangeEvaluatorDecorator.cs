dd34b183ab7612fbf22af942cb8a3545a8cdd91f
Jakub Puchała <puchala.czwa2@hotmail.com>
2016-11-28 14:00:01 +0100

    Time zone should be changable for any language

---
+++ TQL/TQL.Common/Evaluators/TimeZoneChangeEvaluatorDecorator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using TQL.Interfaces;
+
+namespace TQL.Common.Evaluators
+{
+    public class TimeZoneCronForwardFireTimeEvaluatorDecorator : IFireTimeEvaluator
+    {
+        private readonly TimeZoneInfo destinationZoneInfo;
+        private readonly IFireTimeEvaluator evaluator;
+
+        public TimeZoneCronForwardFireTimeEvaluatorDecorator(TimeZoneInfo destinationZoneInfo, IFireTimeEvaluator evaluator)
+        {
+            this.destinationZoneInfo = destinationZoneInfo;
+            this.evaluator = evaluator;
+        }
+
+        public DateTimeOffset? NextFire()
+        {
+            var generatedTime = evaluator.NextFire();
+
+            if (generatedTime.HasValue)
+                return TimeZoneInfo.ConvertTime(generatedTime.Value, destinationZoneInfo);
+            return generatedTime;
+        }
+    }
+}
